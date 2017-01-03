ad7a623d642bd213459d014e7c38ba84141ebe5a
jouler <522051359@qq.com>
2017-01-03 15:05:04 +0800

    2017测试git

--- app/src/main/java/com/qfxa/github1117/MainActivity.java
+++ app/src/main/java/com/qfxa/github1117/MainActivity.java
@@ -1,13 +1,17 @@
 package com.qfxa.github1117;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView test ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = (TextView) findViewById(R.id.test);
+        test.setText("测试成功");
     }
 }
